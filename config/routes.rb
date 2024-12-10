cced5bcff1d0ce51fc419f1371c98d143167d531
gupiragupio <suzukikouhei2020@gmail.com>
2024-12-10 13:42:26 +0900

    new,create アクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :habits, only: :index
+  resources :habits, only: [:index, :new, :create]
   root "habits#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
